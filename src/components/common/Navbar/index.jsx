5e43fd2a133a3cc7c5b7e9a34be5620c46259026
Parthmasala <96944164+Parthmasala@users.noreply.github.com>
2024-03-30 18:51:28 +0530

    fixed User logo problem

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './index.scss';
 import ConnectLogo from '../../../assets/ConnectLogo.png';
 import usericon from '../../../assets/user-icon.png';
@@ -7,17 +7,31 @@ import { FaUserPlus, FaSearch, FaComments } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import ProfilePopup from '../ProfilePopup';
 
-export default function Navbar(currentUser) {
+export default function Navbar({ currentUser }) {
   const navigate = useNavigate();
   const [showPopup, setShowPopup] = useState(false);
+  const userLogoRef = useRef(null);
 
   const Route = (path) => {
     navigate(path);
   };
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (userLogoRef.current && !userLogoRef.current.contains(event.target)) {
+        setShowPopup(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="navbar-master">
-      <img className="connect-logo" src={ConnectLogo} alt="Connect Logo" onClick={() => Route('/home')}/>
+      <img className="connect-logo" src={ConnectLogo} alt="Connect Logo" onClick={() => Route('/home')} />
       <div className="icons">
         <IoMdHome size={40} className="icon-scss" onClick={() => Route('/Home')} />
         <FaUserPlus size={30} className="icon-scss" onClick={() => Route('/Connections')} />
@@ -26,9 +40,9 @@ export default function Navbar(currentUser) {
         <FaComments size={30} className="icon-scss" onClick={() => Route('/Messages')} />
         <IoMdNotifications size={40} className="icon-scss" onClick={() => Route('/Notifications')} />
       </div>
-      <div onMouseEnter={() => setShowPopup(true)} onMouseLeave={() => setShowPopup(false)}>
+      <div ref={userLogoRef} onClick={() => setShowPopup(!showPopup)}>
         <img className="user-logo" src={usericon} alt="User Icon" />
-        {showPopup && <ProfilePopup currentUser={currentUser}/>}
+        {showPopup && <ProfilePopup currentUser={currentUser} />}
       </div>
     </div>
   );
