5f6c4b7e02ea7885cc43d868b673d51418b19618
Parthmasala <96944164+Parthmasala@users.noreply.github.com>
2024-04-23 17:45:46 +0530

    searched-results resposive

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { router } from "./Routes/index.jsx";
 import Footer from "./components/common/Footer/index.jsx";
+import "react-quill/dist/quill.snow.css";
+import { app } from "./FirebaseConfig.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
